dae566e20a66b3275191fb7f61599e7347982a34
Giorgio <mombelligiorgio@gmail.com>
2021-01-05 23:42:25 +0100

    rimozione train load da sched

--- src/models/scheduler.py
+++ src/models/scheduler.py
@@ -1,26 +1,32 @@
 import torch
+from torch.optim import lr_scheduler
 
-def make_scheduler(optimizer, cfg, train_loader):
+def make_scheduler(optimizer, cfg):
 
     if cfg.SOLVER.SCHEDULER == 'CosineAnnealingWarmRestarts':
-        number_of_iteration_per_epoch = len(train_loader)
-        learning_rate_step_size = cfg.SOLVER.SCHEDULER_COS_CPOCH * number_of_iteration_per_epoch
-        
-        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_0=learning_rate_step_size, T_mult=cfg.SOLVER.T_MUL)
+        scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer, 
+            T_0=cfg.SOLVER.SCHEDULER_T0, 
+            T_mult=cfg.SOLVER.T_MUL, 
+            eta_min=cfg.SOLVER.MIN_LR, 
+            last_epoch=-1
+        )
         return scheduler
 
     elif cfg.SOLVER.SCHEDULER == 'ReduceLROnPlateau':
-        return torch.optim.lr_scheduler.ReduceLROnPlateau(
+        return lr_scheduler.ReduceLROnPlateau(
             optimizer=optimizer,
             mode=cfg.SOLVER.SCHEDULER_MODE,
             factor=cfg.SOLVER.SCHEDULER_REDFACT,
             patience=cfg.SOLVER.SCHEDULER_PATIENCE
         )
-    if cfg.SOLVER.SCHEDULER == 'CosineAnnealingLR':
-        number_of_iteration_per_epoch = len(train_loader)
-        learning_rate_step_size = cfg.SOLVER.SCHEDULER_COS_CPOCH * number_of_iteration_per_epoch
-        
-        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_max=cfg.SOLVER.T_MAX, eta_min=cfg.SOLVER.MIN_LR, last_epoch=-1)
+    if cfg.SOLVER.SCHEDULER == 'CosineAnnealingLR':    
+        scheduler = lr_scheduler.CosineAnnealingLR(
+            optimizer, 
+            T_max=cfg.SOLVER.T_MAX, 
+            eta_min=cfg.SOLVER.MIN_LR, 
+            last_epoch=-1
+        )
         return scheduler
     
     else:
