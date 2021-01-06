19c76ba33461a6a977b635553cc11f937ee32b9d
giorgio <mombelligiorgio@gmail.com>
2021-01-06 11:34:40 +0100

    notebook ptl

--- src/models/scheduler.py
+++ src/models/scheduler.py
@@ -1,12 +1,9 @@
 import torch
 
-def make_scheduler(optimizer, cfg, train_loader):
+def make_scheduler(optimizer, cfg):
 
-    if cfg.SOLVER.SCHEDULER == 'CosineAnnealingWarmRestarts':
-        number_of_iteration_per_epoch = len(train_loader)
-        learning_rate_step_size = cfg.SOLVER.SCHEDULER_COS_CPOCH * number_of_iteration_per_epoch
-        
-        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_0=learning_rate_step_size, T_mult=cfg.SOLVER.T_MUL)
+    if cfg.SOLVER.SCHEDULER == 'CosineAnnealingWarmRestarts':      
+        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_0=cfg.SOLVER.SCHEDULER_T0, T_mult=cfg.SOLVER.SCHEDULER_T_MUL)
         return scheduler
 
     elif cfg.SOLVER.SCHEDULER == 'ReduceLROnPlateau':
@@ -14,13 +11,12 @@ def make_scheduler(optimizer, cfg, train_loader):
             optimizer=optimizer,
             mode=cfg.SOLVER.SCHEDULER_MODE,
             factor=cfg.SOLVER.SCHEDULER_REDFACT,
-            patience=cfg.SOLVER.SCHEDULER_PATIENCE
+            patience=cfg.SOLVER.SCHEDULER_PATIENCE,
+            min_lr=cfg.SOLVER.MIN_LR
         )
     if cfg.SOLVER.SCHEDULER == 'CosineAnnealingLR':
-        number_of_iteration_per_epoch = len(train_loader)
-        learning_rate_step_size = cfg.SOLVER.SCHEDULER_COS_CPOCH * number_of_iteration_per_epoch
         
-        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_max=cfg.SOLVER.T_MAX, eta_min=cfg.SOLVER.MIN_LR, last_epoch=-1)
+        scheduler = getattr(torch.optim.lr_scheduler, cfg.SOLVER.SCHEDULER)(optimizer, T_max=cfg.SOLVER.SCHEDULER_T_MAX, eta_min=cfg.SOLVER.MIN_LR, last_epoch=-1)
         return scheduler
     
     else:
